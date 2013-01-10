625704a85f670b931c11c4e35b5d80c55f97945a
NickZhang <looloopig@gmail.com>
2013-01-10 15:48:21 +0800

    Test for having empty box

--- src/Cabinet.rb
+++ src/Cabinet.rb
@@ -2,11 +2,12 @@ class Cabinet
   
   def initialize(capacity)
     @capacity = capacity
+    @usedBoxes = 0
   end
   
   public
   def hasEmptyBox
-    true
+    @usedBoxes < @capacity
   end
   
 end
--- test/CabinetTest.rb
+++ test/CabinetTest.rb
@@ -2,8 +2,13 @@ require 'minitest/autorun'
 require '../src/Cabinet'
 
 class CabinetTest < MiniTest::Unit::TestCase
-  def testShouldTellWetherHasEmptyBox
+  def testShouldHaveEmptyBoxWhenHasCapacity
     cabinet = Cabinet.new(1)
     assert cabinet.hasEmptyBox
+  end
+  
+  def testShouldNotHaveEmptyBoxWhenNoCapacity
+    cabinet = Cabinet.new(0)
+    assert !cabinet.hasEmptyBox
   end
 end
