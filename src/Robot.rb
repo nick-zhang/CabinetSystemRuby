05ee2c4ff4da988e821a8b1ff5372fd15ef99ed0
NickZhang <looloopig@gmail.com>
2013-01-15 14:50:52 +0800

    Robot can store bag

--- src/Robot.rb
+++ src/Robot.rb
@@ -13,4 +13,21 @@ class Robot
     false
   end
   
+  def store bag
+    cabinet = findCabinetWithEmptyBox
+    
+    return nil if cabinet.nil?
+    
+    return cabinet.store(bag)
+  end
+  
+  private 
+  def findCabinetWithEmptyBox
+    @cabinets.each do |cabinet|
+      return cabinet if cabinet.hasEmptyBox
+    end
+    nil
+  end
+  
+  
 end
--- test/RobotTest.rb
+++ test/RobotTest.rb
@@ -1,20 +1,29 @@
 require 'minitest/autorun'
 require '../src/Robot'
 require '../src/Cabinet'
+require '../src/Bag'
 
 class RobotTest < MiniTest::Unit::TestCase
-   def testShouldHaveEmptyBoxWhenHasCapacity
-     cabinet = Cabinet.new(1)
-     cabinets = [cabinet]
-     robot = Robot.new(cabinets)
-     assert robot.hasEmptyBox
-   end
-   
-   def testShouldNotHaveEmptyBoxWhenNoCapacity
-       cabinet = Cabinet.new(0)
-       cabinets = [cabinet]
-       robot = Robot.new(cabinets)
-       assert !robot.hasEmptyBox
-   end
+  def testShouldHaveEmptyBoxWhenHasCapacity
+    cabinet = Cabinet.new(1)
+    cabinets = [cabinet]
+    robot = Robot.new(cabinets)
+    assert robot.hasEmptyBox
+  end
+
+  def testShouldNotHaveEmptyBoxWhenNoCapacity
+    cabinet = Cabinet.new(0)
+    cabinets = [cabinet]
+    robot = Robot.new(cabinets)
+    assert !robot.hasEmptyBox
+  end
+  
+  def testShouldStoreBagWhenThereIsBoxAvailable
+    cabinet = Cabinet.new(1)
+    cabinets = [cabinet]
+    robot = Robot.new(cabinets)
+    ticket = robot.store(Bag.new())
+    refute_nil ticket
+  end
     
 end
