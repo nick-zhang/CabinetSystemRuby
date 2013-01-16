6ff367860ee00754199197aded66489de419ab19
NickZhang <looloopig@gmail.com>
2013-01-16 11:11:28 +0800

    Robot return nil when pick bag with invalid ticket

--- test/RobotTest.rb
+++ test/RobotTest.rb
@@ -43,5 +43,24 @@ class RobotTest < MiniTest::Unit::TestCase
     pickedBag = robot.pick(ticket)
     assert_equal bag, pickedBag 
   end
+  
+  def testShouldReturnNilGivenUsedTicket
+    cabinet = Cabinet.new(1)
+    cabinets = [cabinet]
+    robot = Robot.new(cabinets)
+    bag = Bag.new()
+    ticket = robot.store(bag)
+    pickedBag = robot.pick(ticket)
+    bag = robot.pick(Ticket.new())  
+    assert_nil bag
+  end
+  
+  def testShouldThrowExceptionGivenNotExistedTicket
+    cabinet = Cabinet.new(1)
+    cabinets = [cabinet]
+    robot = Robot.new(cabinets)
+    bag = robot.pick(Ticket.new())
+    assert_nil bag     
+  end
     
 end
